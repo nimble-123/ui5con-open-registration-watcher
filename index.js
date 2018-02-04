b3163cb13b4ef507ad9afcdad9510370a98e72ba
Nils Lutz <info@nilslutz.de>
2018-02-04 02:42:41 +0100

    🔨 Add simple API & UI5 Frontend

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ require('dotenv').config();
 const debug = require('debug')('ui5con-open-reg-watcher');
 const Crawler = require('crawler');
 
+const express = require('express');
+const app = express();
+
+if (process.env.MAX_CONNECTIONS === undefined) {
+  debug('Environment variable for MAX_CONNECTIONS not set. Please set in .env file and try again!');
+  process.exit(1);
+}
+
+if (process.env.RATE_LIMIT === undefined) {
+  debug('Environment variable for RATE_LIMIT not set. Please set in .env file and try again!');
+  process.exit(1);
+}
+
+if (process.env.SITE_TO_CRAWL === undefined) {
+  debug('Environment variable for SITE_TO_CRAWL not set. Please set in .env file and try again!');
+  process.exit(1);
+}
+
 const iMaxConnections = process.env.MAX_CONNECTIONS;
 const iRateLimit = process.env.RATE_LIMIT;
 const sSiteToCrawl = process.env.SITE_TO_CRAWL;
@@ -11,33 +29,65 @@ const crawl = new Crawler({
   rateLimit: iRateLimit
 });
 
-crawl.queue({
-  uri: sSiteToCrawl,
-  callback: (error, res, done) => {
-    if (error) {
-      debug('Error occured during crawl: %s', JSON.stringify(error));
-    } else {
-      const $ = res.$;
-      const sTexts = $.text().toLowerCase();
-      const aLinks = $('a'); //jquery get all hyperlinks
-      let bRegistrationOpen = false;
-      let bEventbriteLinkFound = false;
-      $(aLinks).each(function(i, link) {
-        const sLink = $(link).attr('href');
-        if (sLink && sLink.search('eventbrite.de') !== -1) {
-          debug($(link).text() + ':\n' + $(link).attr('href'));
-          bEventbriteLinkFound = true;
-        }
-      });
-      if (sTexts.search('registration') !== -1) {
-        bRegistrationOpen = true;
-      }
-      if (bRegistrationOpen && bEventbriteLinkFound) {
-        debug('Registration seems to be open!!! :)');
-      } else {
-        debug('Registration seems not to be open :(');
-      }
-    }
-    done();
-  }
+const data = {
+  registrationStatus: '',
+  timestamp: 0
+};
+
+app.use('/', express.static('public'));
+app.get('/api/v1/checkRegistration', (req, res) => {
+  updateData().then((data) => {
+    res.json(data);
+  });
 });
+
+updateData().then((data) => {
+  app.listen(3000, () => {
+    debug('Server listening on port 3000...');
+  });
+});
+
+function updateData() {
+  return new Promise((resolve, reject) => {
+    crawl.queue({
+      uri: sSiteToCrawl,
+      callback: (error, res, done) => {
+        if (error) {
+          debug('Error occured during crawl: %s', JSON.stringify(error));
+          reject(error);
+          return;
+        } else {
+          const $ = res.$;
+          const sTexts = $.text().toLowerCase();
+          const aLinks = $('a'); //jquery get all hyperlinks
+          let bRegistrationOpen = false;
+          let bEventbriteLinkFound = false;
+          $(aLinks).each(function(i, link) {
+            const sLink = $(link).attr('href');
+            if (sLink && sLink.search('eventbrite.de') !== -1) {
+              debug($(link).text() + ':\n' + $(link).attr('href'));
+              bEventbriteLinkFound = true;
+            }
+          });
+          if (sTexts.search('registration') !== -1) {
+            bRegistrationOpen = true;
+          }
+          if (bRegistrationOpen && bEventbriteLinkFound) {
+            data.registrationStatus = 'open';
+            data.timestamp = new Date().getTime();
+            debug('Registration seems to be open!!! :)');
+            debug(data);
+            resolve(data);
+          } else {
+            data.registrationStatus = 'not open';
+            data.timestamp = new Date().getTime();
+            debug('Registration seems not to be open :(');
+            debug(data);
+            resolve(data);
+          }
+        }
+        done();
+      }
+    });
+  });
+}
---
+++ public/controller/App.controller.js
@@ -0,0 +1,67 @@
+sap.ui.define(
+  ['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel', 'sap/m/MessageBox'],
+  function(Controller, JSONModel, MessageBox) {
+    'use strict';
+    return Controller.extend('io.nlsltz.ui5con.watcher.controller.App', {
+      epochToDate: function(sTimestamp) {
+        return new Date(sTimestamp);
+      },
+
+      onInit: function() {
+        this.oModel = new JSONModel();
+        this.getView().setModel(this.oModel);
+        this._fetchData();
+
+        const that = this;
+        let timerId = setTimeout(
+          function tick() {
+            this._fetchData();
+            this._determineRegistrationStatusStyle();
+
+            timerId = setTimeout(tick.bind(this), 5000);
+          }.bind(this),
+          5000
+        );
+      },
+
+      handleRefreshPress: function(oEvent) {
+        this._fetchData();
+      },
+
+      _determineRegistrationStatusStyle: function() {
+        const oStatusText = this.byId('statusText');
+        const sRegistrationStatus = this.oModel.getProperty('/registrationStatus');
+        if (sRegistrationStatus === 'not open') {
+          oStatusText.removeStyleClass('greenText');
+          oStatusText.addStyleClass('redText');
+        } else if (sRegistrationStatus === 'open') {
+          oStatusText.removeStyleClass('redText');
+          oStatusText.addStyleClass('greenText');
+        }
+      },
+
+      _fetchData: function() {
+        var that = this;
+        this.getView().setBusy(true);
+        fetch('/api/v1/checkRegistration')
+          .then(function(res) {
+            if (!res.ok) {
+              throw new Error('HTTP Error: ' + res.statusText);
+            }
+            return res.json();
+          })
+          .then(
+            function(data) {
+              that.getView().setBusy(false);
+              that.oModel.setData(data);
+            },
+            function(error) {
+              that.getView().setBusy(false);
+              jQuery.sap.log.error(error);
+              MessageBox.alert('Unexpected error: ' + error.message);
+            }
+          );
+      }
+    });
+  }
+);
